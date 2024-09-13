42e1bc2d5c47faba19890700dac70b03b02fc173
Theneau Maxime <theneau.maxime@gmail.com>
2024-09-13 19:02:50 +0200

    Feat: install font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,37 @@
 import {NextIntlClientProvider} from 'next-intl';
 import {useRouter} from 'next/router';
 import '../styles/globals.scss';
-import { Roboto } from 'next/font/google'
+import { Rubik, Protest_Guerrilla } from 'next/font/google'
  
-const roboto = Roboto({
+const roboto = Rubik({
   weight: '400',
   subsets: ['latin'],
 })
-
+const title = Protest_Guerrilla({
+  weight: '400',
+  subsets: ['latin'],
+})
 
 export default function App({Component, pageProps}) {
   const router = useRouter();
  
   return (
+    <>
+    <style jsx global>{`
+      :root {
+        --font-heading: ${title.style.fontFamily};
+        --font-body: ${roboto.style.fontFamily};
+      }
+    `}</style>
     <NextIntlClientProvider
       locale={router.query.locale || 'fr'}
       timeZone="Europe/Vienna"
       messages={pageProps.messages}
     >
-      <main className={roboto.className} >
-
+      <main >
         <Component {...pageProps} />
       </main>
     </NextIntlClientProvider>
+    </>
   );
 }
