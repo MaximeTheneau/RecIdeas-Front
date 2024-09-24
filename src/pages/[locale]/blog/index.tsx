234ede66a3e49b00c81bb316467e3ebb9ec5bdc3
Theneau Maxime <theneau.maxime@gmail.com>
2024-09-24 15:10:03 +0200

    Feat: create page index Blog

--- src/pages/[locale]/blog/index.tsx
+++ src/pages/[locale]/blog/index.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable quote-props */
+import Head from 'next/head';
 import type {
   GetStaticPaths,
   GetStaticProps,
@@ -8,14 +9,49 @@ import Cards from '@/components /cards/cards';
 import fetcher from '../../../utils/fetcher';
 // import CategoryPage from '../../../components/category/CategoryPage';
 
-export default function Home({ articles } : any) {
+export default function Home({ articles, page } : any) {
   return (
-    <section>
-      {/* --Articles--*/}
-      <div>
-        <Cards cards={articles} />
-      </div>
-    </section>
+    <>
+      <Head>
+        <title>{page.heading}</title>
+        <meta name="description" content={page.metaDescription} />
+        {/* Open Graph */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={page.heading} />
+        <meta property="og:url" content={`${process.env.NEXT_PUBLIC_URL}/${page.locale}/blog`} />
+        <meta property="og:description" content={page.metaDescription} />
+        <meta property="og:site_name" content="RecIdeas" />
+        <meta property="og:image" content={`${page.imgPost}?format=jpeg`} />
+        <meta property="og:image:width" content="720" />
+        <meta property="og:image:height" content="720" />
+        <meta name="twitter:title" content={page.heading} />
+        <meta name="twitter:description" content={page.metaDescription} />
+        <meta property="twitter:image" content={`${page.imgPost}?format=jpeg`} />
+        <meta property="twitter:creator" content="@RecIdeas" />
+        <meta property="twitter:image:alt" content={page.altImg || page.heading} />
+        <link
+          rel="canonical"
+          href={`${process.env.NEXT_PUBLIC_URL}/${page.locale}/blog`}
+          key="canonical"
+        />
+        {/* Image Preload */}
+        <link
+          rel="preload"
+          as="image"
+          imageSrcSet={page.srcset || ''}
+          imageSizes="100w"
+          fetchPriority="high"
+        />
+      </Head>
+      <section>
+        <h1 className="w-full sm:w-2/3">{page.heading}</h1>
+        <div dangerouslySetInnerHTML={{ __html: page.contents }} />
+        {/* --Articles--*/}
+        <div>
+          <Cards cards={articles} />
+        </div>
+      </section>
+    </>
   );
 }
 export const getStaticProps: GetStaticProps = async (context: GetStaticPropsContext) => {
@@ -29,19 +65,21 @@ export const getStaticProps: GetStaticProps = async (context: GetStaticPropsCont
 
   const { locale } = params;
 
-  // const posts = await fetcher(`${process.env.NEXT_PUBLIC_API_URL}posts/${locale}/blog`);
-  const pageData = await fetcher(`${process.env.NEXT_PUBLIC_API_URL}posts/category/blog`);
-  const translation = pageData.filter(
+  const recypeData = await fetcher(`${process.env.NEXT_PUBLIC_API_URL}posts/category/blog`);
+  const page = await fetcher(`${process.env.NEXT_PUBLIC_API_URL}posts/draft/${params.locale}/azeblog11ea`);
+
+  const translation = recypeData.filter(
     (translationFind:
       { locale: string | string[] | undefined }) => translationFind.locale === locale,
   );
-  const translationFr = pageData.filter(
+  const translationFr = recypeData.filter(
     (translationFind:
       { locale: string | string[] | undefined }) => translationFind.locale === 'fr',
   );
   return {
     props: {
       articles: translation,
+      page,
       translationFr,
       messages: (await import(`../../../../messages/${locale}.json`)).default,
     },
