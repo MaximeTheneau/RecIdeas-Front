85d11864a612552c99f2ed2850862c3f05ee9cf8
Theneau Maxime <theneau.maxime@gmail.com>
2024-09-30 13:10:50 +0200

    Feat: Change navbar accessibility

--- src/components /navbar/Navbar.tsx
+++ src/components /navbar/Navbar.tsx
@@ -7,7 +7,7 @@ import { MdFlatware } from 'react-icons/md';
 export default function Navbar() {
   const t = useTranslations('link');
   return (
-    <nav className="flex justify-between item-center content-center items-center w-full z-10 fixed h-auto font-bold  backdrop-blur ">
+    <nav className="pr-8 flex justify-between item-center content-center items-center w-full z-10 fixed h-auto font-bold  backdrop-blur ">
       <div className="m-4  hover:text-primary">
         <Link href={t('15-recype-link')}>
           <MdFlatware className="w-8 inline-block" />
@@ -21,7 +21,7 @@ export default function Navbar() {
           dismissOnClick={false}
           label={<TbMessageLanguage />}
           color="light"
-          className="p-2 m-2 hover:bg-gray-200 hover:text-black bg-gray-200 active:bg-violet-700 focus:outline-none"
+          className="p-2 mr-4 hover:bg-gray-200 hover:text-black bg-gray-200 active:bg-violet-700 focus:outline-none"
         >
           <Dropdown.Item as={Link} href="/">Fr</Dropdown.Item>
           <Dropdown.Item as={Link} href="/en">En</Dropdown.Item>
