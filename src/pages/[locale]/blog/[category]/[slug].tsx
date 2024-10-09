66ed2523b198a208db2b39acb32f2828828cad29
Theneau Maxime <theneau.maxime@gmail.com>
2024-10-09 11:34:07 +0200

    Feat: change seo url blog og:url

--- src/pages/[locale]/blog/[category]/[slug].tsx
+++ src/pages/[locale]/blog/[category]/[slug].tsx
@@ -36,7 +36,7 @@ export default function Page({ page, translations, pageUrlDefault }: PageProps) 
         {/* Open Graph */}
         <meta property="og:type" content="website" />
         <meta property="og:title" content={page.heading} />
-        <meta property="og:url" content={`${process.env.NEXT_PUBLIC_URL}/${page.locale}`} />
+        <meta property="og:url" content={`${process.env.NEXT_PUBLIC_URL}/${page.url}`} />
         <meta property="og:description" content={page.metaDescription} />
         <meta property="og:site_name" content="RecIdeas" />
         <meta property="og:image" content={`${page.imgPost}?format=jpeg`} />
